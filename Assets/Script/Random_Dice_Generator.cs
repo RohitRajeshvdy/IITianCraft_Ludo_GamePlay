82fc25cda69699f1163115db6abf8f10f7548d9b
Rohit R <150901602+RohitRajeshvdy@users.noreply.github.com>
2025-07-04 20:06:45 +0530

    Dice_Generator
    Created a basic dice generator with can be generated using WASD keys

---
+++ Assets/Script/Random_Dice_Generator.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Random_Dice_Generator : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI diceNumberYellow;
+    [SerializeField] private TextMeshProUGUI diceNumberBlue;
+    [SerializeField] private TextMeshProUGUI diceNumberRed;
+    [SerializeField] private TextMeshProUGUI diceNumberGreen;
+
+    void Update()
+    {
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            diceNumberYellow.text = Random.Range(1, 7).ToString();
+        }
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            diceNumberBlue.text = Random.Range(1, 7).ToString();
+        }
+        if (Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            diceNumberRed.text = Random.Range(1, 7).ToString();
+        }
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            diceNumberGreen.text = Random.Range(1, 7).ToString();
+        }
+    }
+}
+
